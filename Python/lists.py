4e749062f23e47729aee04955e130d697c4feb9f
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2025-06-30 18:27:37 +0200

    Lists Part 2

--- Python/lists.py
+++ Python/lists.py
@@ -5,3 +5,15 @@ print(fruits[1])
 fruits[1] = "blueberry"
 
 print(fruits[1])
+
+fruits.append("kiwi")
+print(fruits)
+
+fruits.insert(1, "orange")
+print(fruits)
+
+fruits.remove("kiwi")
+print(fruits)
+
+fruits.sort()
+print(fruits)
