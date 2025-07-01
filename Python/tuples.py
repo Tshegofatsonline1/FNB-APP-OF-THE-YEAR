b191ca05137e032f3e00994aaa14b89712109d01
Tshegofatsonline1 <magometshegofatso7@gmail.com>
2025-07-01 15:18:37 +0200

    sets2

---
+++ Python/tuples.py
@@ -0,0 +1,15 @@
+my_tuples = (1, 2, 3, 4, 5)
+print(my_tuples)
+
+print(my_tuples[1])
+print(my_tuples[2])
+print(my_tuples[-1])
+
+tuple1 = (1,2,3)
+tuple2 = (4,5,6)
+
+conc_tuples = tuple1 + tuple2
+print(conc_tuples)
+
+rep_tuple = tuple1 * 2
+print(rep_tuple)
